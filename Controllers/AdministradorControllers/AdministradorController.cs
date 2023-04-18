f43e27174b2b4846804f576b0f2c48495b18069f
Sergio <sergio91@hotmail.es>
2023-04-17 21:20:23 -0600

    Vista para administrar empleados

---
+++ Controllers/AdministradorControllers/AdministradorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoParaTuPerro.Controllers.AdministradorControllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Empleados()
+        {
+            return View();
+        }
+    }
+}
