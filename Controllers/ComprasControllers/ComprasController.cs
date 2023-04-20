a4a65528b9d91e30f18d282cb04b5512693b12e8
BELLO303 <dbellop@toluca.tecnm.mx>
2023-04-20 10:54:32 -0600

    Cambios con Nueva Compra

---
+++ Controllers/ComprasControllers/ComprasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoParaTuPerro.Controllers.ComprasControllers
+{
+    public class ComprasController : Controller
+    {
+        public IActionResult Compras()
+        {
+            return View();
+        }
+
+        public IActionResult Productos()
+        {
+            return View();
+        }
+
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+
+        public IActionResult HacerCompra()
+        {
+            return View();
+        }
+    }
+}
