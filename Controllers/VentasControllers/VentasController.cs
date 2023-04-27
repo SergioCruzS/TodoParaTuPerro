881a2e7727c12c580ba29553fc6a03d0e95680b4
Nikiba00 <iblasaub@gmail.com>
2023-04-27 10:33:32 -0600

    CommitIanVentas

---
+++ Controllers/VentasControllers/VentasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoParaTuPerro.Controllers.VentasControllers
+{
+    public class VentasController : Controller
+    {
+        public IActionResult HomeVentas()
+        {
+            return View();
+        }
+        public IActionResult HistorialVentas()
+        {
+            return View();
+        }
+        public IActionResult Devoluciones()
+        {
+            return View();
+        }
+        public IActionResult AtencionClientes()
+        {
+            return View();
+        }
+    }
+}
