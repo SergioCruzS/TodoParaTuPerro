586d0b66bb75a81b9e428a1034550860fe9d65c2
JairoAguilar98 <aguilar.uribe.jairodavid@gmail.com>
2023-05-04 09:36:38 -0600

    Interfaces de Catalogo

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoParaTuPerro.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Catalogo()
+        {
+            return View();
+        }
+    }
+}
