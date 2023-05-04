d94d53fe006e4fda1f0e803de9e8a52df6bd1ca7
JairoAguilar98 <aguilar.uribe.jairodavid@gmail.com>
2023-05-04 09:34:26 -0600

    Interfaces de Registro e Inicio de sesion

---
+++ Controllers/RegistroControllers/RegistroController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoParaTuPerro.Controllers.RegistroControllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarDatos()
+        {
+            return View();
+        }
+
+        public IActionResult OlvidarContraseña()
+        {
+            return View();
+        }
+
+        public IActionResult CodigoRecuperacion()
+        {
+            return View();
+        }
+
+        public IActionResult CambiarContraseña()
+        {
+            return View();
+        }
+
+        public IActionResult MensajeDeCambio()
+        {
+            return View();
+        }
+
+    }
+}
