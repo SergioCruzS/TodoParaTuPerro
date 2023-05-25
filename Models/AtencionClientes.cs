8a75a325e13bdcd8e42fae45326109ec3a74b811
Nikiba00 <iblasaub@gmail.com>
2023-05-25 11:08:49 -0600

    cambios Ventas

---
+++ Models/AtencionClientes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoParaTuPerro.Models
+{
+    public class AtencionClientes
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string NombreNome { get; set; }
+        [Required]
+        public string Correo { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+    }
+}
---
+++ Models/Ventas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoParaTuPerro.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int idDevoluciones { get; set; }
+        [Required]
+        public int idVenta { get; set; }
+        [Required]
+        public string Producto { get; set; }
+        [Required]
+        public string Marca { get; set;}
+        [Required]
+        public string Fecha { get; set;}
+        [Required]
+        public string idCliente { get; set; }
+        [Required]
+        public string Detalles { get; set;}
+    }
+}
